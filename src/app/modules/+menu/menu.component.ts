610ea6e5d6e8785227301537e97a0154a0ddb0dc
FranInfante <infante.portero@gmail.com>
2024-12-06 16:36:00 +0100

    menu done

--- src/app/modules/+menu/menu.component.ts
+++ src/app/modules/+menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LOCATIONS } from '../../shared/constants';
 
 @Component({
   selector: 'app-menu',
@@ -8,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+
+  LOCATIONS: typeof LOCATIONS = LOCATIONS;
+
+  constructor(private router: Router) {}
+
+  navigateToWeighIns() {
+    this.router.navigate([LOCATIONS.weighins]); 
+  }
+
+  navigateToWorkouts() {
+    this.router.navigate([LOCATIONS.workouts]); 
+  }
 
 }
