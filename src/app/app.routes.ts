d21cd0327ca8c30fcbedddc4a4e85071b12c370c
FranInfante <infante.portero@gmail.com>
2024-12-06 16:16:15 +0100

    added menu route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './modules/logreg/+login/login.component';
 import { RegisterComponent } from './modules/logreg/register/register.component';
+import { MenuComponent } from './modules/+menu/menu.component';
 
 export const routes: Routes = [
     {path: "", component: LoginComponent},
-    {path: "register", component: RegisterComponent}
+    {path: "register", component: RegisterComponent},
+    {path: "menu", component: MenuComponent}
 ];
